095a2be9edfe504313b48b9851c19feeae513b6b
lihao012 <1599200622@qq.com>
2019-10-18 15:00:47 +0800

    git send local repository

---
+++ gitssm/src/com/lihao/git/Demo.java
@@ -0,0 +1,13 @@
+package com.lihao.git;
+
+/**
+ * @author 浩梦溪
+ * @site www.lihao.com
+ * @company xxx公司
+ * @create  2019-10-18 14:56
+ */
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("git ider text");
+    }
+}
